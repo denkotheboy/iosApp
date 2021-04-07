36ce4992947d8e4f2467eb521b10b07c079950fa
Denis <kondratev_dv@sima-land.ru>
2021-04-07 20:32:43 +0500

    Update

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useRef, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import update from "immutability-helper";
+
+import {
+  Icon,
+  Button,
+  Header,
+  Input,
+  ListItem,
+  Avatar,
+} from "react-native-elements";
 
 export default function App() {
+  const inputRef = useRef();
+  const [value, setValue] = useState("Введите сообщение");
+  const [listToDos, setListToDos] = useState([]);
+
+  const add = () => {
+    setListToDos([...listToDos, value.trim()]);
+    inputRef.current.clear();
+  };
+  const deleteToDo = (id) => {
+    setListToDos(update(listToDos, { $splice: [[id, 1]] }));
+  };
+
+  useEffect(() => {
+    console.log(listToDos);
+  }, [listToDos]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header
+        backgroundColor="#06baba"
+        leftComponent={{ icon: "menu", color: "#fff" }}
+        centerComponent={{
+          text: "Моё первое приложение",
+          style: { color: "#fff" },
+        }}
+        rightComponent={{ icon: "home", color: "#fff" }}
+      />
+      <Input
+        inputStyle={{ marginTop: 5 }}
+        ref={inputRef}
+        placeholder="Введите задание"
+        onChangeText={(value) => setValue(value)}
+        onSubmitEditing={add}
+      />
+
+      <View style={{ alignItems: "center", padding: 0 }}>
+        <Button
+          title="Отправить"
+          onPress={add}
+          buttonStyle={{ width: "100%", backgroundColor: "#06baba" }}
+        />
+      </View>
+
+      {listToDos.map((toDo, i) => (
+        <ListItem key={i} bottomDivider>
+          <Icon name="angle-right" type="font-awesome" color="#06baba" />
+          <ListItem.Content>
+            <ListItem.Subtitle>{toDo}</ListItem.Subtitle>
+          </ListItem.Content>
+          <Icon
+            name="times-circle"
+            type="font-awesome"
+            color="#f00"
+            onPress={() => deleteToDo(i)}
+          />
+        </ListItem>
+      ))}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    color: "#000",
   },
 });
